4c0d50588d65703dac283b0bc57f8207347b24e9
faisalqureshi05 <faisal199605@gmail.com>
2023-08-08 14:53:02 +0500

    --------

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -3,8 +3,10 @@ const allowedOrigins = require("./allowedOrigins")
 const corsOptions = {
   origin: (origin, callback) => {
     if (allowedOrigins.indexOf(origin) !== -1) {
+      console.log("**** Cors true")
       callback(null, true)
     } else {
+      console.log("**** Cors false")
       callback(new Error("Not allowed by CORS"))
     }
   },
--- server.js
+++ server.js
@@ -1,41 +1,43 @@
-require('dotenv').config()
-const express = require('express')
-const { default: mongoose } = require('mongoose')
-require('express-async-errors')
-const cookieParser = require('cookie-parser')
-const { logger } = require('./middleware/logger')
-const connectDB = require('./config/dbConn')
-const errorHandler = require('./middleware/errorHandler')
-const corsOptions = require('./config/corsOptions')
-const cors = require('cors')
+require("dotenv").config()
+const express = require("express")
+const { default: mongoose } = require("mongoose")
+require("express-async-errors")
+const cookieParser = require("cookie-parser")
+const { logger } = require("./middleware/logger")
+const connectDB = require("./config/dbConn")
+const errorHandler = require("./middleware/errorHandler")
+const corsOptions = require("./config/corsOptions")
+const cors = require("cors")
 
 const app = express()
 const PORT = process.env.PORT || 3500
-connectDB();
+connectDB()
 
 console.log(process.env.NODE_ENV)
 
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors(corsOptions))
-app.use(logger)
-app.use('/user', require('./routes/userRoute'))
-app.use('/note', require('./routes/noteRoute'))
-app.use('/auth', require('./routes/authRoute'))
+if (process.env.NODE_ENV === "developement") {
+  app.use(logger)
+}
+app.use("/user", require("./routes/userRoute"))
+app.use("/note", require("./routes/noteRoute"))
+app.use("/auth", require("./routes/authRoute"))
 
-app.all('*', (req, res) => {
-    res.status(404)
-    res.json("404 not found")
+app.all("*", (req, res) => {
+  res.status(404)
+  res.json("404 not found")
 })
 
 console.log("******************************************************* comment")
 app.use(errorHandler)
 
-mongoose.connection.once('open', () => {
-    console.log("Database connected successFully")
-    app.listen(PORT, () => console.log(`Server is runing on ${PORT}`))
+mongoose.connection.once("open", () => {
+  console.log("Database connected successFully")
+  app.listen(PORT, () => console.log(`Server is runing on ${PORT}`))
 })
 
-mongoose.connection.on('error', (error) => {
-    console.log("Mongoose Error : ", error)
+mongoose.connection.on("error", (error) => {
+  console.log("Mongoose Error : ", error)
 })
